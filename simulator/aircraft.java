3ec8539c38150f2e52362711d0ad7ffa65905f78
Amira Nazri <anazri@hotmail.com>
2020-09-10 11:24:07 +0200

    skeleton files for tower and aircraft.

---
+++ simulator/aircraft.java
@@ -0,0 +1,28 @@
+
+public abstract class Aircraft {
+
+    protected long id {
+
+    }
+
+    protected name {
+
+    }
+
+    protected coordinates {
+
+    }
+
+    private static long idCounter {
+
+    }
+
+    private Aircraft (name, coordinates) {
+
+    }
+
+    private long nextId {
+
+    }
+
+}
---
+++ simulator/tower.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Tower {
+    private List<Flyable> observers = new ArrayList<Flyable>();
+
+    public Register(flyable : Flyable) void {
+
+    }
+
+    public Unregister(flyable : Flyable) void {
+
+    }
+
+    protected conditionsChanged() void {
+
+    }
+
+}
